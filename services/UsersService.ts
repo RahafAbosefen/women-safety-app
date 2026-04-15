40852c846009ca6652c6a0c362a8aeeb1156bbe7
Rahaf Abosefen <s12114971@stu.najah.edu>
2026-04-16 02:37:28 +0300

    create a profile page

--- services/UsersService.ts
+++ services/UsersService.ts
@@ -1,9 +1,7 @@
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 import app from "./firebaseConfig";
 
-
 const db = getFirestore(app);
-
 
 export const UsersService = {
   createUserProfile: async (uid: string, data: any) => {
@@ -17,5 +15,18 @@ export const UsersService = {
       throw error;
     }
   },
-  
+  getUserProfile: async (uid: string) => {
+    try {
+      const userDoc = await getDoc(doc(db, "users", uid));
+      if (userDoc.exists()) {
+        return userDoc.data();
+      } else {
+        console.log("No such user!");
+        return null;
+      }
+    } catch (error) {
+      console.error("Firestore Error (Get):", error);
+      throw error;
+    }
+  },
 };
